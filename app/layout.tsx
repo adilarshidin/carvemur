d6a8933c8fd86f5fa43b63a1109b1c766dd66f22
adilarshidin <adil.therunningman@gmail.com>
2026-07-14 13:18:54 +0200

    Render sticky Header in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ReactLenis } from "lenis/react";
+import Header from "./components/Header";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,7 +31,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ReactLenis root>{children}</ReactLenis>
+        <ReactLenis root>
+          <Header />
+          {children}
+        </ReactLenis>
       </body>
     </html>
   );
